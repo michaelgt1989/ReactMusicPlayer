aa5090e81782691ec982a52518e5f94c2df26b06
Michael George-Taylor <michael.gt1989@gmail.com>
2021-11-30 06:24:58 -0700

    installation of modules

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './App.css';
+import LandingMenu from './LandingMenu'
+import axios from './axios'
+import SongSelection from './SongSelection'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App;
