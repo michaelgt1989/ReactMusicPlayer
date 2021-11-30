55aa7c3edca0da27b596cae3eb3826aa2ae2fdb6
Michael George-Taylor <michael.gt1989@gmail.com>
2021-11-30 09:44:55 -0700

    Commented out section to test

--- src/Components/SearchSong.jsx
+++ src/Components/SearchSong.jsx
@@ -9,35 +9,36 @@ import React from 'react';
     handleSubmit = (event) =>{
         this.props.searchSong(this.state);
     }
+//commenting out for research. Attempted pop-up button to search for songs.
 
-const searchSong = () =>{
-    return(
-        <Modal 
-            show={this.props.isOpen} 
-            onHide={this.props.closeModel} >
-            <Modal.Header closeButton>
-            <Modal.Title>Temp Holding Title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-              <Form.Group >
-                  <Form.Label>Title: </Form.Label>
-                  <Form.Control name= 'title' type="text" onChange={this.handleChange} value={this.state.title} placeholder={this.state.title}/>           
-                  <Form.Label>Artist: </Form.Label>
-                  <Form.Control name = 'artist' type="text" onChange={this.handleChange} value={this.state.artist} placeholder={this.state.artist}/>           
-                  <Form.Label>Album: </Form.Label>
-                  <Form.Control name = 'album' type="text" onChange={this.handleChange} value={this.state.album} placeholder={this.state.album}/>           
-                  <Form.Label>Release Date: </Form.Label>
-                  <Form.Control name = 'release_date' type="datetime-local" onChange={this.handleChange} value={this.state.release_date} placeholder={this.state.release_date}/>           
-                  <Form.Label>Genre: </Form.Label>
-                  <Form.Control name = 'genre' type="text" onChange={this.handleChange} value={this.state.genre} placeholder={this.state.genre}/>           
-              </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-              <Button variant="primary" type="submit" onClick={() => this.props.handleSubmit(this.state.name)}>
-                  Submit
-              </Button>
-          </Modal.Footer>
-        </Modal>
-        )
-        }
-export default searchSong;
+// const searchSong = () =>{
+//     return(
+//         <Modal 
+//             show={this.props.isOpen} 
+//             onHide={this.props.closeModel} >
+//             <Modal.Header closeButton>
+//             <Modal.Title>Temp Holding Title</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//               <Form.Group >
+//                 <Form.Label>Song: </Form.Label>
+//                 <Form.Control name= 'title' type="text" onChange={this.handleChange} value={this.state.title} placeholder={this.state.title}/>           
+//                 <Form.Label>Artist: </Form.Label>
+//                 <Form.Control name = 'artist' type="text" onChange={this.handleChange} value={this.state.artist} placeholder={this.state.artist}/>           
+//                 <Form.Label>Album: </Form.Label>
+//                 <Form.Control name = 'album' type="text" onChange={this.handleChange} value={this.state.album} placeholder={this.state.album}/>           
+//                 <Form.Label>Release Date: </Form.Label>
+//                 <Form.Control name = 'release_date' type="datetime-local" onChange={this.handleChange} value={this.state.release_date} placeholder={this.state.release_date}/>           
+//                 <Form.Label>Genre: </Form.Label>
+//                 <Form.Control name = 'genre' type="text" onChange={this.handleChange} value={this.state.genre} placeholder={this.state.genre}/>           
+//             </Form.Group>
+//         </Modal.Body>
+//         <Modal.Footer>
+//             <Button variant="primary" type="submit" onClick={() => this.props.handleSubmit(this.state.name)}>
+//                 Submit
+//             </Button>
+//         </Modal.Footer>
+//     </Modal>
+//     )
+//     }
+// export default searchSong;
--- src/Components/SubmitSong.jsx
+++ src/Components/SubmitSong.jsx
@@ -18,29 +18,29 @@ handleChange = (event) =>{
     this.setState({
         [event.target.name]: event.target.value
         })
-    }
+}
 
-    handleSubmit = (event) =>{
-        this.props.submitSong(this.state);
-    }
+handleSubmit = (event) =>{
+    this.props.submitSong(this.state);
+}
 
-    render() { 
-        return ( 
-        <form onSubmit = {this.handleSubmit} className='label'>
-            <label>Song Title</label>
-            <input name = 'artist' onChange = {this.handleChange} value = {this.state.artist}/>
-            <label>Album</label>
-            <input name= 'title' onChange = {this.handleChange} value = {this.state.title}/> 
-            <label>Artist</label>
-            <input name = 'album' onChange = {this.handleChange} value = {this.state.album}/>
-            <label>Release Date</label>
-            <input type = 'datetime-local' name = 'release_date' onChange = {this.handleChange} value = {this.state.release_date}/>
-            <label>Genre</label>
-            <input name = 'genre' onChange = {this.handleChange} value = {this.state.genre}/>
-            <hr />
-            <button type = 'submit' >Add a song</button>
+render() { 
+    return ( 
+    <form onSubmit = {this.handleSubmit} className='label'>
+        <label>Song Title</label>
+        <input name = 'artist' onChange = {this.handleChange} value = {this.state.artist}/>
+        <label>Album</label>
+        <input name= 'title' onChange = {this.handleChange} value = {this.state.title}/> 
+        <label>Artist</label>
+        <input name = 'album' onChange = {this.handleChange} value = {this.state.album}/>
+        <label>Release Date</label>
+        <input type = 'datetime-local' name = 'release_date' onChange = {this.handleChange} value = {this.state.release_date}/>
+        <label>Genre</label>
+        <input name = 'genre' onChange = {this.handleChange} value = {this.state.genre}/>
+        <hr />
+        <button type = 'submit' >Add a song</button>
         </form>
-         );
+        );
     }
 }
  
